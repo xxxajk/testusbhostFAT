aa7dc88cda5ddbb0d10f8d4b1e53155300f26344
Andrew J. Kroll <a@oo.ms>
2014-05-18 23:55:14 -0400

    Update debugging, add ARM debugging

---
+++ arm-debug.sh
@@ -0,0 +1,5 @@
+#
+# Note! OpenOCD must be running for this to work!
+#
+
+ddd --debugger "arm-none-eabi-gdb -x gdb.conf"
---
+++ avr-debug.sh
@@ -0,0 +1,5 @@
+#
+# Note! avarice must be running for this to work!
+#
+
+ddd --debugger "avr-gdb -x gdb.conf"
